31cad25d6582c7d66e8cf1955ac0d7ab129894f8
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-01-30 07:36:12 +0000

    add worksheet part 2

--- academykatas/src/main/scala/Kata/kata.scala
+++ academykatas/src/main/scala/Kata/kata.scala
@@ -2,9 +2,13 @@ package Kata
 
 object kata extends App {
 
+  def makeList (Number: Int) : List[Int] = {
+    val y = (0 to Number).toList.reverse
+    y
+  }
+  println(makeList(5))
+
+
   val x = List.fill(3)("Hello")
   println(x)
-
-
-
 }
